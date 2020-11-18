2d7e9e9741c7c8e91388c0e96f9edf781dcd65a7
gidizil <gidizil@gmail.com>
2020-11-18 12:02:08 +0200

    finished Dataset

--- python_scripts/data_utils.py
+++ python_scripts/data_utils.py
@@ -3,7 +3,11 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import shutil
-from torch.utils import data
+from torch.utils.data import Dataset, DataLoader
+import ast
+from PIL import Image
+import torch
+import torchvision
 
 
 class Utils:
@@ -39,9 +43,9 @@ class Utils:
                                 sep=':', names=['img_name', 'annotations'])
         train_df, val_df = train_test_split(annots_df, test_size=test_rate)
         train_df.to_csv(os.path.join(train_path, 'labels.txt'),
-                        sep=':', index=False, header=False)
+                        sep=':', index=False, header=True)
         val_df.to_csv(os.path.join(validation_path, 'labels.txt'),
-                      sep=':', index=False, header=False)
+                      sep=':', index=False, header=True)
 
         # move images to train and val dirs
         train_imgs_list = train_df.img_name.tolist()
@@ -59,15 +63,54 @@ class Utils:
             shutil.copyfile(curr_img_path, new_img_path)
 
 
-
-class BusesDataset(data.dataset):
+class BusesDataset(Dataset):
     """Dataset to load images to the model"""
-    pass
+    def __init__(self, root, folder='train', transforms=None, f_name='labels.txt'):
+        self.transforms = []
+        if transforms is not None:
+            self.transforms.append(transforms)
+
+        self.root = root
+        self.folder = folder
+        # TODO: Make it better later - No pandas
+        xy_df = pd.read_csv(os.path.join(root, folder, f_name), sep=':')
+        xy_df['annotations'] = xy_df['annotations'].apply(lambda x: ast.literal_eval('[' + x + ']'))
+        xy_df['box_data'] = xy_df['annotations'].apply(lambda x: [y[0:4] for y in x])
+        xy_df['labels'] = xy_df['annotations'].apply(lambda x: [y[4] for y in x])
+        self.box_data = xy_df['box_data'].values
+        self.labels = xy_df['labels'].values
+        self.imgs = xy_df['img_name'].values
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root, self.folder, self.imgs[idx])
+        img = Image.open(img_path).convert('RGB')
+        bbox = self.box_data[idx]
+        label = self.labels[idx]
+
+        for transform in self.transforms:
+            transform(img)
 
 
-# TODO: add stuff for config - perhaps another class
-u = Utils()
-u.split_imgs_train_val('busesTrain')
+        targets = {}
+        targets['bbox'] = torch.from_numpy(bbox).double()
+        targets['label'] = torch.from_numpy(label).type(torch.int64)
+
+        return img.double(), targets
+
+# u = Utils()
+# u.split_imgs_train_val('busesTrain')
+
+
+root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+dataset = BusesDataset(root, 'train', transforms=torchvision.transforms.ToTensor())
+
+print(dataset[0])
+
+
+
 
 
 
