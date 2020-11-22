bf5c732137d33825ee396ac5c02df05e8d9f7efa
gidizil <gidizil@gmail.com>
2020-11-22 11:47:58 +0200

    support multiple GPU, Towards evaluating loss correctly

--- models/faster_rcnn.py
+++ models/faster_rcnn.py
@@ -1,7 +1,7 @@
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 import torchvision
 import torch
-
+import torch.nn as nn
 
 class FasterRCNNMODEL:
     #TODO: Later on enable passing params params
@@ -24,6 +24,11 @@ class FasterRCNNMODEL:
         num_classes = 7
         in_features = self.model.roi_heads.box_predictor.cls_score.in_features
         self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+        # Allow Multiple GPUs:
+        # if torch.cuda.device_count() > 1:
+        #     self.model = nn.DataParallel(self.model)
+
         self.model = self.model.to(device)
 
         if self.params is None:
@@ -34,10 +39,16 @@ class FasterRCNNMODEL:
 
         self.optimizer = torch.optim.SGD(params, lr=0.01)
 
-    def train_model(self, train_dataloader, num_epochs):
+    def train_model(self, train_loader, num_epochs):
+        """
+        Train (only!) of the model
+        :param train_loader: DataLoader object
+        :param num_epochs: int. Number of epochs to train the model
+        :return: None,
+        """
         self.model.train()  # Set to training mode
         for epoch in range(num_epochs):
-            for images, targets in train_dataloader:
+            for images, targets in train_loader:
                 images = list(image.to(self.device) for image in images)
                 targets = [{k: v.to(self.device) for k, v in t.items()} for t in targets]
 
@@ -54,7 +65,51 @@ class FasterRCNNMODEL:
                 # Update weights
                 self.optimizer.step()
 
-            print('Loss = {:.4f}'.format(losses.item()))
+            print('Train Loss = {:.4f}'.format(losses.item()))
+
+    def train_eval_model(self, train_loader, val_loader, num_epochs):
+        """
+        Train model and evaluate performance after each epoch
+        :param train_loader: DataLoader object. Training images and targets
+        :param val_loader: DataLoader object. validation images and targets
+        :param num_epochs: int. Number of epochs for training and validation
+        :return:
+        """
+
+        for epoch in range(num_epochs):
+            self.model.train()  # Set to training mode
+            with torch.set_grad_enabled(True):
+                for images, targets in train_loader:
+                    # Pass data to GPU
+                    images = list(image.to(self.device) for image in images)
+                    targets = [{k: v.to(self.device) for k, v in t.items()} for t in targets]
+
+                    # Zero Gradients
+                    self.optimizer.zero_grad()
+
+                    # self.model = self.model.double()
+
+                    # Calculate Loss
+                    loss_dict = self.model(images, targets)  # what happens here?
+                    losses = sum(loss for loss in loss_dict.values())
+
+                    # Backward Prop & Update weights
+                    losses.backward()
+                    self.optimizer.step()
+
+                print('Train Loss = {:.4f}'.format(losses.item()))
+
+            # TODO: Calculate Dice and IoU loss for it
+            # self.model.eval() # Set model to evaluate performance
+            # with torch.no_grad():
+            #     for images, targets in val_loader:
+            #         images = list(image.to(self.device) for image in images)
+            #         targets = [{k: v.to(self.device) for k, v in t.items()} for t in targets]
+            #
+            #         loss_dict = self.model(images, targets)
+            #         # losses = sum(loss for loss in loss_dict.values())
+            #
+            #     print('Validation Loss = {:.4f}'.format(losses.item()))
 
 
 
--- python_scripts/our_main.py
+++ python_scripts/our_main.py
@@ -9,18 +9,30 @@ once we are pleased with it we'll
 integrate it to the necessary files
 """
 
+# Set DataLoaders for train and validation
 root = root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 buses_train_dataset = BusesDataset(root, 'train',
                                    f_name='labels.txt', faster_rcnn=True,
                                    transforms=torchvision.transforms.ToTensor())
+buses_val_dataset = BusesDataset(root, 'validation',
+                                 f_name='labels.txt', faster_rcnn=True,
+                                 transforms=torchvision.transforms.ToTensor())
 
 train_loader = DataLoader(buses_train_dataset,
-                          batch_size=4, shuffle=True,
-                          collate_fn=lambda x: list(zip(*x)))  # Understand better collate_fn
+                          batch_size=8, shuffle=True,
+                          collate_fn=lambda x: list(zip(*x)),
+                          num_workers=16)  # Understand better collate_fn
+
+val_loader = DataLoader(buses_val_dataset,
+                        batch_size=8, shuffle=True,
+                        collate_fn=lambda x: list(zip(*x)),
+                        num_workers=16)
 
 """ 1. Try Faster RCNN """
 faster_rcnn_model = FasterRCNNMODEL()
 faster_rcnn_model.set_model()
-faster_rcnn_model.train_model(train_loader, num_epochs=7)
+# faster_rcnn_model.train_model(train_loader, num_epochs=7)
 # TODO: ADD predict_model
+
+faster_rcnn_model.train_eval_model(train_loader, val_loader, num_epochs=5)
 # TODO: Add tensorboard support
