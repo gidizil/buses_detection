733281e7ce4e2e20eb399f25e0ef0601897bc770
gidizil <gidizil@gmail.com>
2020-11-29 11:41:33 +0200

    minor fix

--- models/faster_rcnn.py
+++ models/faster_rcnn.py
@@ -139,7 +139,6 @@ class FasterRCNNMODEL:
                         targets = self.model(images)
                         MiscUtils.view(images, targets, k=len(images), model_type='faster_rcnn')
 
-                # TODO: convert bbox from [x1, y1, x2, y2] to [x1, y1, w, h]
                 DataUtils.gen_out_file('output_file.txt', imgs_name_list, bbox_list, labels_list)
                 print('Validation Loss = {:.4f}'.format(val_loss / len(val_loader.dataset)))
 
